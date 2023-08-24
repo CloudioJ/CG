35eddbe87d0c630ae0d67fa301d96c25a192869b
Claudio J <48624347+CloudioJ@users.noreply.github.com>
2023-08-24 17:53:12 -0300

    Tentativa

--- README.md
+++
@@ -1,3 +0,0 @@
-# CG
-
-Trabalho 1: Mover uma câmera por um cenário a partir de uma curva de Bezier
---
+++ Trabalho1/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:688d6c2ef2f017f8e0f5b22ad73b52a80f3ec094bc15c95f6899845d6e54cb09
+size 28995
